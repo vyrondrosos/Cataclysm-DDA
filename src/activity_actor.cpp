0834069cf3102979b56490a849bcf6dcfff06da4
Vyron Drosos <vyrondrosos@gmail.com>
2026-06-03 16:49:34 +0300

    Keep laser designation activity turn-based

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -13640,6 +13640,7 @@ namespace
 
 constexpr int laser_designator_charges_per_interval = 5;
 constexpr time_duration laser_designator_charge_interval = 10_seconds;
+constexpr time_duration laser_designator_activity_interval = 1_seconds;
 
 void clear_laser_designation( Character &who )
 {
@@ -13741,8 +13742,8 @@ bool can_use_mounted_laser_designator( const Character &who, map &here,
 
 void laser_designator_activity_actor::start( player_activity &act, Character & )
 {
-    act.moves_total = calendar::INDEFINITELY_LONG;
-    act.moves_left = calendar::INDEFINITELY_LONG;
+    act.moves_total = to_moves<int>( laser_designator_activity_interval );
+    act.moves_left = act.moves_total;
     next_charge = calendar::turn;
 }
 
@@ -13801,8 +13802,8 @@ void laser_designator_activity_actor::do_turn( player_activity &act, Character &
     write_laser_designation( who, current_target->pos, success );
     last_target_pos = current_target->pos;
     target_pos = current_target->pos;
-    who.pause();
-    act.moves_left = calendar::INDEFINITELY_LONG;
+    act.moves_total = to_moves<int>( laser_designator_activity_interval );
+    act.moves_left = act.moves_total;
 }
 
 void laser_designator_activity_actor::finish( player_activity &, Character &who )
