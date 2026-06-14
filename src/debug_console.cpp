8acb096d6fdb61b3b0f205eb4197c728052a3b66
Vyron Drosos <vyrondrosos@gmail.com>
2026-06-14 13:46:23 +0300

    Fix drone feedback issues

--- src/debug_console.cpp
+++ src/debug_console.cpp
@@ -4108,6 +4108,8 @@ const char *timed_event_type_name( timed_event_type t )
             return "fpv_drone_recovered_message";
         case timed_event_type::FPV_DRONE_LOST_MESSAGE:
             return "fpv_drone_lost_message";
+        case timed_event_type::FPV_DRONE_SCOUT_READY_MESSAGE:
+            return "fpv_drone_scout_ready_message";
         case timed_event_type::FPV_DRONE_IMPACT_MESSAGE:
             return "fpv_drone_impact_message";
         case timed_event_type::FPV_DRONE_PAYLOAD_DROP:
